6a2ba7db1190355161b4da3e9fe4978c8ed0b6f3
Name <arkanagames7@gmail.com>
2026-05-07 19:36:10 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,60 @@
+from pygame import *
+
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, player_speed, width, height):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (width, height))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+
+
+class RightPlat(GameSprite):
+    def update(self):
+        keys = key.get_pressed()
+        if keys[K_UP] and self.rect.y > 5:
+            self.rect.y -= self.speed
+
+        if keys[K_DOWN] and self.rect.y < win_height - 120:
+            self.rect.y += self.speed
+
+
+
+# Persons
+rplat = RightPlat('blue.jpg', 600, 200, 3, 10, 100)
+
+
+# Scene
+win_width = 700
+win_height = 500
+window = display.set_mode((win_width, win_height))
+display.set_caption('Ping-Pong')
+background = transform.scale(image.load('background.jpg'), (win_width, win_height))
+
+
+
+game = True
+finish = False
+clock = time.Clock()
+FPS = 60
+
+
+# Cycle
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+    
+    if finish != True:
+        window.blit(background, (0, 0))
+        rplat.update()
+
+        rplat.reset()
+
+    display.update()
+    clock.tick(FPS)
